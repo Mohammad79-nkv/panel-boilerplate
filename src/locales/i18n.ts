7fa31b707d8fac4201c340bdc33624d82c9d7b0c
mohammad <mohammadnv79@gmail.com>
2023-09-25 20:34:15 +0330

    add i18next config

---
+++ src/locales/i18n.ts
@@ -0,0 +1,68 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import en from '../translations/main-en.json';
+import fa from '../translations/main-fa.json';
+import { ConvertedToObjectType } from './types';
+
+const translationsJson = {
+  fa: {
+    translation: fa,
+  },
+  en: {
+    translation: en,
+  },
+};
+
+export type TranslationResource = typeof fa;
+export type LanguageKey = keyof TranslationResource;
+
+export const translations: ConvertedToObjectType<TranslationResource> =
+  {} as any;
+
+/*
+ * Converts the static JSON file into an object where keys are identical
+ * but values are strings concatenated according to syntax.
+ * This is helpful when using the JSON file keys and still have the intellisense support
+ * along with type-safety
+ */
+const convertLanguageJsonToObject = (obj: any, dict: {}, current?: string) => {
+  Object.keys(obj).forEach(key => {
+    const currentLookupKey = current ? `${current}.${key}` : key;
+    if (typeof obj[key] === 'object') {
+      dict[key] = {};
+      convertLanguageJsonToObject(obj[key], dict[key], currentLookupKey);
+    } else {
+      dict[key] = currentLookupKey;
+    }
+  });
+};
+
+export const i18n = i18next
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init(
+    {
+      keySeparator: false,
+      resources: translationsJson,
+      lng: 'fa',
+      fallbackLng: 'fa',
+      debug:
+        process.env.NODE_ENV !== 'production' &&
+        process.env.NODE_ENV !== 'test',
+
+      interpolation: {
+        escapeValue: false, // not needed for react as it escapes by default
+      },
+    },
+    () => {
+      convertLanguageJsonToObject(fa, translations);
+    },
+  );
--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -1,28 +1,28 @@
 import { useTranslation } from "react-i18next"
 import {  useQuery } from '@tanstack/react-query';
-import {CustomerService} from '@/services/customer/customer-service'
+// import {CustomerService} from '@/services/customer/customer-service'
 import {useState , useEffect} from 'react'
 
-const customerService = new CustomerService();
+// const customerService = new CustomerService();
 
 function Dashboard() {
     const {t} = useTranslation();
     const [usersList , setUsersList] = useState<any>([]);
 
-    const query = useQuery(['usersList'], async () => await customerService.getListWithOutToken());
+    // const query = useQuery(['usersList'], async () => await customerService.getListWithOutToken());
 
-      useEffect(() => {
-            console.log('usersList' , usersList , query)
-            if(query?.data?.data?.length !== 0){
-                setUsersList(query?.data?.data!) 
-            }
-      }, [query])
+    //   useEffect(() => {
+    //         console.log('usersList' , usersList , query)
+    //         if(query?.data?.data?.length !== 0){
+    //             setUsersList(query?.data?.data!) 
+    //         }
+    //   }, [query])
       
 
-      if(query.isLoading) {
-        return <>
-        ...loading</>
-      }
+    //   if(query.isLoading) {
+    //     return <>
+    //     ...loading</>
+    //   }
     return (
         <>
             dashboard
